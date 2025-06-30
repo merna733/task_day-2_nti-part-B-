b7808fcf19ca29accab0d6a3a63da6e33e1020dc
Merna_elgohary <mero777444@gmail.com>
2025-06-30 23:43:48 +0300

    Add files via upload

---
+++ TextStyle.js
@@ -0,0 +1,35 @@
+function ChangeFont(font) {
+    document.getElementById("text").style.fontFamily = font;
+}
+
+function ChangeAlign(align) {
+    document.getElementById("text").style.textAlign = align;
+}
+
+function ChangeHeight(height) {
+    document.getElementById("text").style.lineHeight = height;
+}
+
+function ChangeLSpace(space) {
+    document.getElementById("text").style.letterSpacing = space;
+}
+
+function ChangeIndent(indent) {
+    document.getElementById("text").style.textIndent = indent;
+}
+
+function ChangeTransform(transform) {
+    document.getElementById("text").style.textTransform = transform;
+}
+
+function ChangeDecorate(decorate) {
+    document.getElementById("text").style.textDecoration = decorate;
+}
+
+function ChangeBorder(border) {
+    document.getElementById("text").style.borderStyle = border;
+}
+
+function ChangeBorderColor(color) {
+    document.getElementById("text").style.borderColor = color;
+}
